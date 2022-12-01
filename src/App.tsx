51c68922b418d600a6f2ea9973bc72c31f079ffa
arsyadn <arsyadnurzaky@gmail.com>
2022-12-01 11:22:43 +0700

    create inputtask.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import InputTask from "./components/InputTask";
 
 const App: React.FC = () => {
+  const [list, setList] = useState<string>("");
+  console.log(list);
+
   return (
     <div className="app-wrapper">
       <p>TASKS-KU</p>
-      <InputTask />
+      <InputTask list={list} setList={setList} />
     </div>
   );
 };
--- src/components/InputTask.tsx
+++ src/components/InputTask.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "./styles.css";
 
-const InputTask: React.FC = () => {
+interface Props {
+  list: string;
+  setList: React.Dispatch<React.SetStateAction<string>>;
+}
+const InputTask: React.FC<Props> = ({ list, setList }) => {
   return (
     <div className="input-container">
       <div className="input-wrapper">
-        <input type="text" placeholder="Enter a task" className="input-task" />
+        <input
+          type="text"
+          placeholder="Enter a task"
+          className="input-task"
+          value={list}
+          onChange={(e) => setList(e.target.value)}
+        />
         <button className="btn-go">GO</button>
       </div>
     </div>
